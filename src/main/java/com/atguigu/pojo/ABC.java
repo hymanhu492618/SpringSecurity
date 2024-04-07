e43553a0a5dbae3133b4c23b34de643025f26088
Hyman <287813689@qq.com>
2024-04-07 11:24:10 +0800

    dev分支新增了一个ABC类

---
+++ src/main/java/com/atguigu/pojo/ABC.java
@@ -0,0 +1,16 @@
+package src.main.java.com.atguigu.pojo;
+
+/**
+ * ClassName:ABC
+ * Package:src.main.java.com.atguigu.pojo
+ * Description:
+ *
+ * @Author:
+ * @Create: 2024/4/7 11:20
+ * @Version:
+ */
+public class ABC {
+    public static void main(String[] args) {
+        System.out.println("这个是dev分支的代码");
+    }
+}
