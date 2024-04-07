dc1eb1f3564761e5754971602c9abff039c1a77d
Hyman <287813689@qq.com>
2024-04-07 11:38:43 +0800

    修改了dev分支的代码

--- src/main/java/com/atguigu/pojo/ABC.java
+++ src/main/java/com/atguigu/pojo/ABC.java
@@ -11,6 +11,6 @@ package src.main.java.com.atguigu.pojo;
  */
 public class ABC {
     public static void main(String[] args) {
-        System.out.println("这个是dev分支的代码");
+        System.out.println("这个是dev分支修改后的代码");
     }
 }
